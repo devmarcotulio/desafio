975117ad144ddcee7a0450f093761e0f3830fa6c
Marco Túlio <marcotulio_iub@hotmail.com>
2025-04-09 09:21:47 -0300

    ajuste para pegar apenas as transações dos ultimos 60segundos

--- src/domain/usecases/StatisticsTransactionUseCase.ts
+++ src/domain/usecases/StatisticsTransactionUseCase.ts
@@ -1,3 +1,4 @@
+import { Transaction } from "../entities/Transaction";
 import { TransactionRepository } from "../repositories/TransactionRepository";
 
 export class StatisticsTransactionUseCase {
@@ -5,15 +6,22 @@ export class StatisticsTransactionUseCase {
 
   async execute() {
     const transactions = await this.transactionRepository.getAll();
-    if (transactions.length === 0) {  
+    const recentTransactions = transactions.filter(({ dataHora }) => {
+      return new Date().getTime() - new Date(dataHora).getTime() <= 60 * 1000
+    });
+    if (recentTransactions.length === 0) {
       return {
         count: 0,
         sum: 0,
         avg: 0,
         min: 0,
         max: 0,
-      }
+      };
     }
+    return this.calculateStats(recentTransactions);
+  }
+
+  private calculateStats(transactions: Transaction[]) {
     const count = transactions.length;
     let sum = 0;
     let min = transactions[0].valor;
@@ -29,7 +37,7 @@ export class StatisticsTransactionUseCase {
       sum: Number(sum.toFixed(2)),
       avg: Number(avg.toFixed(2)),
       min: Number(min.toFixed(2)),
-      max: Number(max.toFixed(2))
-    }
+      max: Number(max.toFixed(2)),
+    };
   }
 }
